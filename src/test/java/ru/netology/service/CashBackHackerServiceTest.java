549478ab2304b82df2658259501cedbbbd9ecd04
Dmitry Tarasov <vizalyyy@gmail.com>
2024-12-18 00:44:19 +0500

    With TestNG

---
+++ src/test/java/ru/netology/service/CashBackHackerServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashBackHackerServiceTest {
+
+    CashBackHackerService service = new CashBackHackerService();
+
+    @Test
+    public void remainLowerLimit() {
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void remainUpLowerLimit() {
+        int amount = 1;
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void remainMiddleLimit() {
+        int amount = 500;
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void remainLowUpperLimit() {
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void remainUpperLimit() {
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+}
